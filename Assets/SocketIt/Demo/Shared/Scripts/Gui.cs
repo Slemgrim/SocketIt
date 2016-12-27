c2dd069aeb4fd2f683fa2c405ff92e0b95c62c54
markus.ritberger <markus.ritberger@karriere.at>
2016-12-27 14:01:33 +0100

    update sidebar design

---
+++ Assets/SocketIt/Demo/Shared/Scripts/Gui.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+namespace SocketIt.Examples {
+	public class Gui : MonoBehaviour {
+
+		public string startScene = "Intro";
+		private List<LoadSceneOnClick> sceneItems;
+
+		public Text Title;
+		public Text Description;
+
+		public void Awake(){
+			DontDestroyOnLoad(transform.gameObject);
+			sceneItems = new List<LoadSceneOnClick> (GetComponentsInChildren<LoadSceneOnClick>());
+		}
+
+		public void Start(){
+			SceneManager.LoadScene(startScene);
+			SceneManager.sceneLoaded += OnSceneLoad;
+		}
+
+		private void OnSceneLoad(Scene scene, LoadSceneMode mode){
+			foreach (LoadSceneOnClick sceneItem in sceneItems) {
+				sceneItem.UpdateMenu ();
+			}
+		}
+
+		public void SetDescription(string title, string description){
+			Title.text = title;
+			Description.text = description;
+		}
+	}
+}
--- Assets/SocketIt/Demo/Shared/Scripts/LoadSceneOnClick.cs
+++ Assets/SocketIt/Demo/Shared/Scripts/LoadSceneOnClick.cs
@@ -1,18 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace SocketIt.Examples
 {
     public class LoadSceneOnClick : MonoBehaviour
     {
 
-        public string Scene;
+        public string scene;
+		public Color activeColor;
+		public Color inactiveColor;
+		public Gui gui;
+
+		public string title;
+		public string description;
+
+		private Text text;
+
+		void Awake(){
+			text = GetComponentInChildren<Text> ();
+
+			UpdateMenu ();
+		}	
+
+		public void UpdateMenu(){
+			if ((SceneManager.GetActiveScene ().name == SceneManager.GetSceneByName (scene).name)) {
+				SetActive ();
+			} else {
+				SetInactive ();
+			}
+		}
 
         public void OnClick()
         {
-            SceneManager.LoadScene(Scene);
+            SceneManager.LoadScene(scene);
+			UpdateMenu ();
         }
+
+		private void SetActive(){
+			text.color = activeColor;
+			gui.SetDescription (title, description);
+		}
+
+		private void SetInactive(){
+			text.color = inactiveColor;
+		}
 
     }
 }
